ee18dbd9a273d4ef8c4564fa0364699fb298ba94
Ivona Cvija <ivca.cvija@gmail.com>
2025-03-13 22:04:15 +0000

    fixed up server.js for different data

--- kotlin_onnx_backend/server.js
+++ kotlin_onnx_backend/server.js
@@ -17,12 +17,12 @@ mongoose.connect(MONGO_URI, {})
 
 // Test Route on browser on phone
 app.get("/", (req, res) => {
-    res.send("Server is running and connected to MongoDB Atlas!");
+    res.send("Server is running and is connected to MongoDB Atlas!");
 });
 
 // POST for receiving logs from Android App
 app.post('/logs', (req, res) => {
-    console.log('Received log data:', req.body);
+    console.log('Received log data:', JSON.stringify(req.body, null, 2));
     res.status(200).json({ message: 'Log received successfully' });
 });
 
