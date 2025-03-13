e729f4d8a8f0d272f5504f85888b392b3ef343f6
Ivona Cvija <ivca.cvija@gmail.com>
2025-03-13 20:20:22 +0000

    testing route on network(browser on phone), post method for getting data from android app

--- kotlin_onnx_backend/server.js
+++ kotlin_onnx_backend/server.js
@@ -12,12 +12,18 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // Connect to MongoDB Atlas
 mongoose.connect(MONGO_URI, {})
-.then(() => console.log("✅ MongoDB Connected to Atlas"))
-.catch(err => console.error("❌ MongoDB Connection Error:", err));
+.then(() => console.log("MongoDB Connected to Atlas"))
+.catch(err => console.error("MongoDB Connection Error:", err));
 
-// Test Route
+// Test Route on browser on phone
 app.get("/", (req, res) => {
     res.send("Server is running and connected to MongoDB Atlas!");
+});
+
+// POST for receiving logs from Android App
+app.post('/logs', (req, res) => {
+    console.log('Received log data:', req.body);
+    res.status(200).json({ message: 'Log received successfully' });
 });
 
 // Start the Server
